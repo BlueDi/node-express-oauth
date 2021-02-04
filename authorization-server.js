9cf8c7e34e95e53728c4012c8af38a1e85a0bf88
Diogo Cruz <diogo.cruz@ctw.bmwgroup.com>
2021-02-04 14:38:03 +0000

    2.5 Rendering the login page

--- authorization-server.js
+++ authorization-server.js
@@ -3,39 +3,39 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const jwt = require("jsonwebtoken")
 const {
-	randomString,
-	containsAll,
-	decodeAuthCredentials,
-	timeout,
+    randomString,
+    containsAll,
+    decodeAuthCredentials,
+    timeout,
 } = require("./utils")
 
 const config = {
-	port: 9001,
-	privateKey: fs.readFileSync("assets/private_key.pem"),
+    port: 9001,
+    privateKey: fs.readFileSync("assets/private_key.pem"),
 
-	clientId: "my-client",
-	clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
-	redirectUri: "http://localhost:9000/callback",
+    clientId: "my-client",
+    clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
+    redirectUri: "http://localhost:9000/callback",
 
-	authorizationEndpoint: "http://localhost:9001/authorize",
+    authorizationEndpoint: "http://localhost:9001/authorize",
 }
 
 const clients = {
-	"my-client": {
-		name: "Sample Client",
-		clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
-		scopes: ["permission:name", "permission:date_of_birth"],
-	},
-	"test-client": {
-		name: "Test Client",
-		clientSecret: "TestSecret",
-		scopes: ["permission:name"],
-	},
+    "my-client": {
+        name: "Sample Client",
+        clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
+        scopes: ["permission:name", "permission:date_of_birth"],
+    },
+    "test-client": {
+        name: "Test Client",
+        clientSecret: "TestSecret",
+        scopes: ["permission:name"],
+    },
 }
 
 const users = {
-	user1: "password1",
-	john: "appleseed",
+    user1: "password1",
+    john: "appleseed",
 }
 
 const requests = {}
@@ -53,25 +53,30 @@ app.use(bodyParser.urlencoded({extended: true}))
 /*
 Your code here
 */
-function authorized(req, res) {
-	const requestID = randomString();
-	requests[requestID] = req.query;
-	res.end();
+function authorized(req, res, client, scope) {
+    const requestId = randomString();
+    requests[requestId] = req.query;
+    const params = {
+        requestId: requestId,
+        client: client,
+        scope: scope,
+    };
+    res.render("login", params);
 }
 
 app.get('/authorize', (req, res) => {
-	const client = clients[req.query.client_id];
-	const scope = req.query.scope;
-	client && scope && containsAll(client.scopes, scope.split(" "))
-		? authorized(req, res)
-		: res.status(401).end();
+    const client = clients[req.query.client_id];
+    const scope = req.query.scope;
+    client && scope && containsAll(client.scopes, scope.split(" "))
+        ? authorized(req, res, client, scope)
+        : res.status(401).end();
 })
 
 const server = app.listen(config.port, "localhost", function () {
-	var host = server.address().address
-	var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 })
 
 // for testing purposes
 
-module.exports = { app, requests, authorizationCodes, server }
+module.exports = {app, requests, authorizationCodes, server}
