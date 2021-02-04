aad1a7b5c4cb70f91eddce02f9a424c912aba275
Diogo Cruz <diogo.cruz@ctw.bmwgroup.com>
2021-02-04 18:02:05 +0000

    3.3 Getting user information from the auth token

--- protected-resource.js
+++ protected-resource.js
@@ -1,50 +1,60 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const fs = require("fs")
-const { timeout } = require("./utils")
+const jwt = require("jsonwebtoken")
+const {timeout} = require("./utils")
 
 const config = {
-	port: 9002,
-	publicKey: fs.readFileSync("assets/public_key.pem"),
+    port: 9002,
+    publicKey: fs.readFileSync("assets/public_key.pem"),
 }
 
 const users = {
-	user1: {
-		username: "user1",
-		name: "User 1",
-		date_of_birth: "7th October 1990",
-		weight: 57,
-	},
-	john: {
-		username: "john",
-		name: "John Appleseed",
-		date_of_birth: "12th September 1998",
-		weight: 87,
-	},
+    user1: {
+        username: "user1",
+        name: "User 1",
+        date_of_birth: "7th October 1990",
+        weight: 57,
+    },
+    john: {
+        username: "john",
+        name: "John Appleseed",
+        date_of_birth: "12th September 1998",
+        weight: 87,
+    },
 }
 
 const app = express()
 app.use(timeout)
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true}))
 
 /*
 Your code here
 */
+function verifyJWT(res, authorization) {
+    const token = authorization.slice(7);
+    jwt.verify(token, config.publicKey, {algorithm: ['RS256']}, (err, decoded) => {
+        decoded
+            ? res.end()
+            : res.status(401).end();
+    });
+}
+
 app.get('/user-info', (req, res) => {
-	const {authorization} = req.headers;
-	authorization
-		? res.end()
-		: res.status(401).end();
+    const {authorization} = req.headers;
+    authorization
+        ? verifyJWT(res, authorization)
+        : res.status(401).end();
 })
 
 const server = app.listen(config.port, "localhost", function () {
-	var host = server.address().address
-	var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 })
 
 // for testing purposes
 module.exports = {
-	app,
-	server,
+    app,
+    server,
 }
